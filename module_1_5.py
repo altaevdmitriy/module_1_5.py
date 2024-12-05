710e298a4f6d7033083d35623b52a68f8d1817ee
АлтаевДмитрий <Baitog1982@gmail.com>
2024-12-05 08:21:10 +0800

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+#immutable_var=tuple()
+#tuple=(1,2,'a','b')
+#print(tuple)
+#tuple[3]=4
+#print(tuple)  кортеж-это не изменяемый тип данных,поэтому  изьенить значение элементов кортежа нельзя
+mutable_list=[1,2,'a','b']
+mutable_list[0]=4
+print(mutable_list)
